cbb2294e18e7e74bb133b57c2dd40dbf629a32be
Gustavo Martíenz <gusmbox@gmail.com>
2021-05-11 10:18:21 +0200

    Added to repository, Excercise 2 from Module 1

---
+++ m1ex2/src/m1ex2/Main.java
@@ -0,0 +1,20 @@
+package m1ex2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Declara una variable double. Assigna-li un valor amb 4 decimals
+		double d = 3.1415;
+		int i = (int) d;
+		float f = (float) d;
+		String s = Double.toString(d);
+		
+		System.out.println("(double) " + d);
+		System.out.println("(int) " + i);
+		System.out.println("(float) " + f);
+		System.out.println("(String) " + s);
+	}
+
+}
